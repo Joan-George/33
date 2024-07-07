0673d21bfc17546962fbc3441c188045c5f167fe
Joan_George_01 <jgtharchius@raisetech.io>
2024-07-07 18:28:58 +0530

    final build completed

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,31 +1,30 @@
 import connectDB from "@/db/Database";
-import ClothingProduct from "@/models/Product";
 import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
-  try {
-    await connectDB();
+	try {
+		await connectDB();
 
-    const categories = await ClothingProduct.distinct("category");
-    return NextResponse.json(
-      {
-        status: "success",
-        data: categories,
-      },
-      {
-        status: 200,
-      }
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      {
-        status: "error",
-        message: error.message,
-      },
-      {
-        status: 500,
-      }
-    );
-  }
+		const categories = [];
+		return NextResponse.json(
+			{
+				status: "success",
+				data: categories,
+			},
+			{
+				status: 200,
+			}
+		);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{
+				status: "error",
+				message: error.message,
+			},
+			{
+				status: 500,
+			}
+		);
+	}
 };
